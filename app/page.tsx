bad68fb9dc09c6e6300348d0b7c7e38fad02c1eb
jinhyuck cha <ckwlsgur20@gmail.com>
2024-01-11 13:30:43 +0900

    init

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>NextJs 13 Server Side Pagination</h1>
+      </div>
+    </section>
+  )
+}
+
+export default Home
---
+++ lib/mongo/movies.ts
@@ -0,0 +1,93 @@
+import { Collection, Db, Document, MongoClient } from 'mongodb'
+import clientPromise from '@/lib/mongo/client'
+
+let client: MongoClient
+let db: Db
+let movies: Collection<Document>
+
+async function init() {
+  if (db) return
+  try {
+    client = await clientPromise
+    db = client.db()
+    movies = db.collection('movies')
+  } catch (error) {
+    throw new Error('Failed to connect to the database.')
+  }
+}
+
+// Avoid exhausting your connection
+// ;(async () => {
+//   await init()
+// })()
+
+/// Movies ///
+
+export const getMovies = async ({
+  query,
+  page = 1,
+  limit = 10
+}: {
+  query?: string
+  page: number
+  limit: number
+}) => {
+  try {
+    if (!movies) await init()
+
+    const skip = (page - 1) * limit
+
+    const pipeline: PipelineStage[] = [{ $skip: skip }, { $limit: limit }]
+
+    if (query) {
+      pipeline.unshift({
+        $search: {
+          index: 'search',
+          text: {
+            query,
+            fuzzy: {
+              maxEdits: 1,
+              prefixLength: 3,
+              maxExpansions: 50
+            },
+            path: {
+              wildcard: '*'
+            }
+          }
+        }
+      })
+    }
+
+    await sleep(1000)
+
+    const result = await movies.aggregate(pipeline).toArray()
+
+    return { movies: result }
+  } catch (error) {
+    return { error }
+  }
+}
+
+async function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+type PipelineStage =
+  | {
+      $search: {
+        index: string
+        text: {
+          query: string
+          fuzzy: {}
+          path: {
+            wildcard: string
+          }
+        }
+      }
+    }
+  | {
+      $skip: number
+    }
+  | {
+      $limit: number
+    }
